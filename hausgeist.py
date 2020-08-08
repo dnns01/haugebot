5916e9ebaf6f2737de955de306c90ade2b554fe3
dnns01 <mail@dnns01.de>
2020-08-08 22:26:21 +0200

    Changed timers from being variables with timestamps to Tasks.

--- hausgeist.py
+++ hausgeist.py
@@ -1,7 +1,9 @@
+import asyncio
 import os
 import time
 
 from dotenv import load_dotenv
+from twitchio.dataclasses import Context, Message, Channel
 from twitchio.ext import commands
 
 load_dotenv()
@@ -18,7 +20,7 @@ PIPI_COLOR_0 = os.getenv("PIPI_COLOR_0")
 PIPI_COLOR_1 = os.getenv("PIPI_COLOR_1")
 PIPI_COLOR_2 = os.getenv("PIPI_COLOR_2")
 PIPI_COLOR_3 = os.getenv("PIPI_COLOR_3")
-pipi_timer = 0
+pipi_task = None
 pipi_votes = {}
 
 # vote bot related
@@ -29,8 +31,8 @@ VOTE_COLOR = os.getenv("VOTE_COLOR")
 VOTE_PLUS = os.getenv("VOTE_PLUS")
 VOTE_MINUS = os.getenv("VOTE_MINUS")
 VOTE_NEUTRAL = os.getenv("VOTE_NEUTRAL")
-vote_first = 0
-vote_last = 0
+vote_end_task = None
+vote_interim_task = None
 votes = {}
 
 bot = commands.Bot(
@@ -50,19 +52,20 @@ def get_percentage(part, total):
 async def notify_pipi(ctx, use_timer=True, message=None):
     """ Write a message in chat, if there hasn't been a notification since PIPI_DELAY seconds. """
 
-    global pipi_timer
-    timestamp = time.time()
+    global pipi_task
 
-    if use_timer and timestamp < pipi_timer + PIPI_DELAY:
+    if use_timer and pipi_task and not pipi_task.done():
         return
 
-    pipi_timer = timestamp
+    if pipi_task:
+        pipi_task.cancel()
+
+    pipi_task = asyncio.create_task(pipi_block_notification())
     vote_ctr = 0
     chatters = await bot.get_chatters(CHANNEL)
 
     if message is not None:
-        await ctx.color(PIPI_COLOR_0)
-        await ctx.send(message)
+        await send_me(ctx, message, PIPI_COLOR_0)
     else:
         for vote in pipi_votes.values():
             if vote == 1:
@@ -70,40 +73,39 @@ async def notify_pipi(ctx, use_timer=True, message=None):
 
         percentage = get_percentage(vote_ctr, chatters.count)
 
-        vote_ctr = 20
-
         if vote_ctr == 0:
-            await ctx.color(PIPI_COLOR_0)
-            await ctx.send_me(f'Kein Druck (mehr) auf der Blase. Es kann fröhlich weiter gestreamt werden!')
+            await send_me(ctx, f'Kein Druck (mehr) auf der Blase. Es kann fröhlich weiter gestreamt werden!',
+                          PIPI_COLOR_0)
         elif vote_ctr == 1:
-            await ctx.color(PIPI_COLOR_1)
-            await ctx.send_me(f'{vote_ctr} ({percentage}%) Mensch müsste mal')
+            await send_me(ctx, f'{vote_ctr} ({percentage}%) Mensch müsste mal', PIPI_COLOR_1)
         elif vote_ctr < PIPI_THRESHOLD_1:
-            await ctx.color(PIPI_COLOR_1)
-            await ctx.send_me(f'{vote_ctr} ({percentage}%) Menschen müssten mal')
+            await send_me(ctx, f'{vote_ctr} ({percentage}%) Menschen müssten mal', PIPI_COLOR_1)
         elif vote_ctr < PIPI_THRESHOLD_2:
-            await ctx.color(PIPI_COLOR_2)
-            await ctx.send_me(f'{vote_ctr} ({percentage}%) Menschen müssten mal haugeAgree')
+            await send_me(ctx, f'{vote_ctr} ({percentage}%) Menschen müssten mal haugeAgree', PIPI_COLOR_2)
         else:
-            await ctx.color(PIPI_COLOR_3)
-            await ctx.send_me(f'{vote_ctr} ({percentage}%) Menschen müssten mal haugeAgree haugeAgree')
+            await send_me(ctx, f'{vote_ctr} ({percentage}%) Menschen müssten mal haugeAgree haugeAgree', PIPI_COLOR_3)
 
 
-async def notify_vote_result(ctx, final_result=False):
+async def notify_vote_result(message, final_result=False):
     votes_list = get_votes()
 
     output = f'{VOTE_PLUS} {votes_list[0][0]} ({votes_list[0][1]}%), ' \
              f'{VOTE_NEUTRAL} {votes_list[1][0]} ({votes_list[1][1]}%), ' \
              f'{VOTE_MINUS} {votes_list[2][0]} ({votes_list[2][1]}%) | '
+    output += f'Endergebnis' if final_result else f'Zwischenergebnis'
 
-    if final_result:
-        output += f'Endergebnis'
-    else:
-        output += f'Zwischenergebnis'
+    await send_me(message, output, VOTE_COLOR)
 
-    await ctx.channel.color(VOTE_COLOR)
-    await ctx.channel.send_me(output)
 
+async def send_me(ctx, content, color):
+    """ Change Text color to color and send content as message """
+
+    if type(ctx) is Context or type(ctx) is Channel:
+        await ctx.color(color)
+        await ctx.send_me(content)
+    elif type(ctx) is Message:
+        await ctx.channel.color(color)
+        await ctx.channel.send_me(content)
 
 @bot.event
 async def event_ready():
@@ -142,8 +144,7 @@ async def cmd_pause(ctx):
 
     if ctx.author.is_mod:
         pipi_votes = {}
-        await ctx.color(PIPI_COLOR_0)
-        await ctx.send_me("Jetzt geht noch mal jeder aufs Klo, und dann streamen wir weiter!")
+        await send_me(ctx, "Jetzt geht noch mal jeder aufs Klo, und dann streamen wir weiter!", PIPI_COLOR_0)
 
 
 @bot.command(name="reset")
@@ -163,55 +164,39 @@ async def cmd_pipimeter(ctx):
 
 
 @bot.event
-async def event_message(ctx):
-    global votes, vote_first, vote_last
+async def event_message(message):
+    global votes
 
-    if ctx.content.startswith(PREFIX):
-        await bot.handle_commands(ctx)
+    if message.content.startswith(PREFIX):
+        await bot.handle_commands(message)
     else:
 
         # make sure the bot ignores itself and nightbot
-        if ctx.author.name.lower() == [NICK.lower(), 'nightbot']:
+        if message.author.name.lower() == [NICK.lower(), 'nightbot']:
             return
 
         # check if message is a vote
-        msg = ctx.content
-        if msg[:2] == '+-' or msg[:2] == '-+' or msg[:3] == '-/+' or msg[:3] == '+/-' or msg[:len(VOTE_NEUTRAL)] == VOTE_NEUTRAL:
-            add_vote(ctx, 'neutral')
+        msg = message.content
+        if msg[:2] == '+-' or msg[:2] == '-+' or msg[:3] == '-/+' or msg[:3] == '+/-' or msg[:len(
+                VOTE_NEUTRAL)] == VOTE_NEUTRAL:
+            add_vote(message, 'neutral')
         elif msg[:1] == '+' or msg[:len(VOTE_PLUS)] == VOTE_PLUS:
-            add_vote(ctx, 'plus')
+            add_vote(message, 'plus')
         elif msg[:1] == '-' or msg[:len(VOTE_MINUS)] == VOTE_MINUS:
-            add_vote(ctx, 'minus')
-
-        # have X seconds passed since last vote? -> post end result
-        if time.time() >= vote_last + VOTE_DELAY_END and vote_first != 0:
-            # not enough votes?
-            if len(votes) >= VOTE_MIN_VOTES:
-                get_votes()
-                await notify_vote_result(ctx, final_result=True)
-
-            vote_first = 0
-            vote_last = 0
-            votes.clear()
-
-        # have X seconds passed since first vote? -> post interim result
-        if time.time() >= vote_first + VOTE_DELAY_INTERIM and vote_first != 0:
-            if len(votes) >= VOTE_MIN_VOTES:
-                vote_first = time.time()
-                await notify_vote_result(ctx)
+            add_vote(message, 'minus')
 
 
 def add_vote(ctx, votetype):
     """adds votes to the votes-dict and sets timestamps"""
-    global votes, vote_first, vote_last
+    global votes, vote_end_task, vote_interim_task
 
-    # is this the first vote?
-    if vote_first == 0:
-        vote_first = time.time()
+    if len(votes) == 0:
+        vote_end_task = asyncio.create_task(vote_end_voting(bot.get_channel(CHANNEL)))
+        vote_interim_task = asyncio.create_task(vote_interim_voting(bot.get_channel(CHANNEL)))
 
-    # should vote count as last (=newest) vote?
+    # should vote extend voting?
     if ctx.author.name not in votes or votes[ctx.author.name] != votetype:
-        vote_last = time.time()
+        vote_end_task.set_name(int(time.time()))
 
     # add vote to dict
     votes[ctx.author.name] = votetype
@@ -235,6 +220,40 @@ def get_votes():
     return [[plus, get_percentage(plus, len(votes))],
             [neutral, get_percentage(neutral, len(votes))],
             [minus, get_percentage(minus, len(votes))]]
+
+
+async def vote_end_voting(channel):
+    """ End a currently open voting """
+
+    # Wait for the initial VOTE_DELAY_END seconds
+    await asyncio.sleep(VOTE_DELAY_END)
+
+    # Check every second, if the delay has finished, because since VOTE_DELAY_END seconds there was no vote,
+    # that extended the voting time
+    while int(vote_end_task.get_name()) + VOTE_DELAY_END >= time.time():
+        await asyncio.sleep(1)
+
+    # if len(votes) >= VOTE_MIN_VOTES:
+    await notify_vote_result(channel, final_result=True)
+
+    votes.clear()
+
+
+async def vote_interim_voting(channel):
+    """ End a currently open voting """
+
+    global vote_interim_task
+
+    await asyncio.sleep(VOTE_DELAY_INTERIM)
+    if not vote_end_task.done() and len(votes) >= VOTE_MIN_VOTES:
+        await notify_vote_result(channel)
+        vote_interim_task = asyncio.create_task(vote_interim_voting(bot.get_channel(CHANNEL)))
+
+
+async def pipi_block_notification():
+    """ Just do nothing but sleep for PIPI_DELAY seconds """
+
+    await asyncio.sleep(PIPI_DELAY)
 
 
 bot.run()
